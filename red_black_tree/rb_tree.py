f43739faeeb9e75574199766c0ebd5983bf3ad04
Hiroaki-Kubo <hiroaki.kubo@pixiedusttech.com>
2022-11-21 23:17:29 +0900

    Fix right rotate

--- red_black_tree/rb_tree.py
+++ red_black_tree/rb_tree.py
@@ -43,7 +43,8 @@ def draw_tree(graph, tree: Node):
     graph.write_png("rb_tree.png")
 
 
-def left_rotate(root: Node, x: Node) -> None:
+def left_rotate(root: Node, x: Node) -> Node:
+    print("left: ", x.key)
     y = x.right
     x.right = y.left
     if y.left is not None:
@@ -57,66 +58,86 @@ def left_rotate(root: Node, x: Node) -> None:
         x.parent.right = y
     y.left = x
     x.parent = y
+    print("left1: ", x.parent.key)
+    return root
 
 
-def right_rotate(root: Node, x: Node) -> None:
-    y = x.left
-    x.left = y.right
-    if y.right is not None:
-        y.right.parent = x
-    y.parent = x.parent
-    if x.parent is None:
-        root = y
-    elif x == x.parent.right:
-        x.parent.right = y
+def right_rotate(root: Node, y: Node) -> Node:
+    x = y.left
+    print("y: ", y.key)
+    print("x: ", x.key)
+    y.left = x.right
+    if x.right is not None:
+        x.right.parent = y
+    x.parent = y.parent
+    if y.parent is None:
+        print("if1")
+        root = x
+    elif y == y.parent.right:
+        print("if2")
+        y.parent.right = x
     else:
-        x.parent.left = y
-    y.right = x
-    x.parent = y
+        print("if3")
+        y.parent.left = x
+    x.right = y
+    y.parent = x
+    print("root: ", root.key)
+    print("root.right: ", root.right.key)
+    print("root.left: ", root.left.key)
+    return root
 
 
 def rb_insert_fixup(root: Node, new_node: Node) -> None:
-    while new_node.parent.color == "red":
+    while new_node.parent != None and new_node.parent.color == "red":
+        print("new_node: ", new_node.key)
         if new_node.parent == new_node.parent.parent.left:
             y = new_node.parent.parent.right
-            if y.color == "red":
+            if y != None and y.color == "red":
+                print("pattern1")
                 new_node.parent.color = "gray"
                 y.color = "gray"
                 new_node.parent.parent.color = "red"
                 new_node = new_node.parent.parent
-                print("new: ", new_node.key)
             elif new_node == new_node.parent.right:
+                print("pattern2")
                 new_node = new_node.parent
-                left_rotate(root, new_node)
+                root = left_rotate(root, new_node)
             else:
+                print("pattern3")
                 new_node.parent.color = "gray"
                 new_node.parent.parent.color = "red"
-                right_rotate(root, new_node.parent.parent)
+                print("pa: ", new_node.parent.parent.key)
+                root = right_rotate(root, new_node.parent.parent)
 
-        # else:
-        #     y = new_node.parent.parent.left
-        #     if y.color == "red":
-        #         new_node.parent.color = "gray"
-        #         y.color = "gray"
-        #         new_node.parent.parent.color = "red"
-        #         new_node = new_node.parent.parent
-        #     elif new_node == new_node.parent.left:
-        #         new_node = new_node.parent
-        #         right_rotate(root, new_node)
-        #     else:
-        #         new_node.parent.color = "gray"
-        #         new_node.parent.parent.color = "red"
-        #         left_rotate(root, new_node.parent.parent)
+        else:
+            y = new_node.parent.parent.left
+            if y != None and y.color == "red":
+                print("pattern4")
+                new_node.parent.color = "gray"
+                y.color = "gray"
+                new_node.parent.parent.color = "red"
+                new_node = new_node.parent.parent
+            elif new_node == new_node.parent.left:
+                print("pattern5")
+                new_node = new_node.parent
+                right_rotate(root, new_node)
+            else:
+                print("pattern6")
+                new_node.parent.color = "gray"
+                new_node.parent.parent.color = "red"
+                print("new_node_granpa: ", new_node.parent.parent.key)
+                left_rotate(root, new_node.parent.parent)
 
     root.color = "gray"
+    return root
 
 
-
-def rb_insert(root: Node, new_node: Node) -> None:
+def rb_insert(root: Node, new_node: Node, nil: Node) -> Node:
     print("new: ", new_node.key)
-    # y = None
+    # y = 
     x = root
     while x != None:
+        # print("x: ", x.key, root.parent.key)
         y = x
         if new_node.key < x.key:
             x = x.left
@@ -133,19 +154,22 @@ def rb_insert(root: Node, new_node: Node) -> None:
     new_node.left = None
     new_node.right = None
     new_node.color = "red"
-    print("new_parent: ", new_node.parent.color)
-    rb_insert_fixup(root, new_node)
+    root = rb_insert_fixup(root, new_node)
+    print("root: ", root.key)
+    return root
 
 
 def rb_tree(data: list):
     # print(data)
-    nil = Node("gray", data[0], None, None, None)
-    root = Node("gray", data[0], None, None, nil)
+    nil = Node("nil", -1, None, None, None)
+    root = Node("gray", data[0], None, None, None)
     for i in range(1, len(data)):
-        new_node = Node("red", data[i], nil, nil, None)
-        print("data: ", data[i])
-        rb_insert(root, new_node)
-    
+        new_node = Node("red", data[i], None, None, None)
+        root = rb_insert(root, new_node, nil)
+    print("root: ", root.key)
+    # print("root.right: ", root.right.key)
+    # print("root.left: ", root.left.key)
+
     # print(root.right.right.right.key)
     graph = pydot.Dot(graph_type = 'graph', strict=True)
     x = pydot.Node(root.key, style="filled", fillcolor=root.color)
@@ -153,10 +177,11 @@ def rb_tree(data: list):
     draw_tree(graph, root)
 
 
-
 def main():
-    # list1 = [1, 2, 4, 5, 7, 8, 11, 14, 15]
-    list2 = [7, 2, 14, 5, 1, 8, 11, 4, 15]
+    # list2 = [7, 2, 14, 5, 1, 8, 11, 4, 15, 17, 18, 19, 20, 21, 23, 24, 25, 30, 35]
+    # list2 = [7, 2, 14, 25, 12, 13, 11, 20, 15, 30]
+    # list2 = [11, 2, 1, 7, 5, 8, 14, 15, 4]
+    list2 = [11, 2, 1, 7, 5, 8, 14, 15]
     rb_tree(list2)
 
 
