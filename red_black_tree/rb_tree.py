bcb82e1b91ef685621a7d55c7e8c0167e4739fe8
Hiroaki-Kubo <hiroaki.kubo@pixiedusttech.com>
2022-11-23 17:29:45 +0900

    Add test

--- red_black_tree/rb_tree.py
+++ red_black_tree/rb_tree.py
@@ -1,6 +1,8 @@
 from hashlib import new
 from typing import Any, Type
 import pydot
+import random
+import copy
 
 
 class Node:
@@ -13,7 +15,6 @@ class Node:
 
 
 def draw_tree(graph, tree: Node):
-
     if tree.left is None and tree.right is None:
         return
     elif tree.left is not None and tree.right is not None:
@@ -44,7 +45,6 @@ def draw_tree(graph, tree: Node):
 
 
 def left_rotate(root: Node, x: Node) -> Node:
-    print("left: ", x.key)
     y = x.right
     x.right = y.left
     if y.left is not None:
@@ -58,33 +58,23 @@ def left_rotate(root: Node, x: Node) -> Node:
         x.parent.right = y
     y.left = x
     x.parent = y
-    print("left1: ", x.parent.key)
-    print("left_root: ", root.key)
     return root
 
 
 def right_rotate(root: Node, y: Node) -> Node:
     x = y.left
-    print("y: ", y.key)
-    print("x: ", x.key)
     y.left = x.right
     if x.right is not None:
         x.right.parent = y
     x.parent = y.parent
     if y.parent is None:
-        print("if1")
         root = x
     elif y == y.parent.right:
-        print("if2")
         y.parent.right = x
     else:
-        print("if3")
         y.parent.left = x
     x.right = y
     y.parent = x
-    print("root: ", root.key)
-    print("root.right: ", root.right.key)
-    print("root.left: ", root.left.key)
     return root
 
 
@@ -93,52 +83,40 @@ def rb_insert_fixup(root: Node, new_node: Node) -> None:
         if new_node.parent == new_node.parent.parent.left:
             y = new_node.parent.parent.right
             if y != None and y.color == "red":
-                print("pattern1")
                 new_node.parent.color = "gray"
                 y.color = "gray"
                 new_node.parent.parent.color = "red"
                 new_node = new_node.parent.parent
             elif new_node == new_node.parent.right:
-                print("pattern2")
                 new_node = new_node.parent
                 root = left_rotate(root, new_node)
             else:
-                print("pattern3")
                 new_node.parent.color = "gray"
                 new_node.parent.parent.color = "red"
-                print("pa: ", new_node.parent.parent.key)
                 root = right_rotate(root, new_node.parent.parent)
 
         else:
             y = new_node.parent.parent.left
             if y != None and y.color == "red":
-                print("pattern4")
                 new_node.parent.color = "gray"
                 y.color = "gray"
                 new_node.parent.parent.color = "red"
                 new_node = new_node.parent.parent
             elif new_node == new_node.parent.left:
-                print("pattern5")
                 new_node = new_node.parent
                 root = right_rotate(root, new_node)
             else:
-                print("pattern6")
                 new_node.parent.color = "gray"
                 new_node.parent.parent.color = "red"
-                print("new_node_granpa: ", new_node.parent.parent.key)
                 root = left_rotate(root, new_node.parent.parent)
 
     root.color = "gray"
-    print("root: ", root.key)
     return root
 
 
-def rb_insert(root: Node, new_node: Node, nil: Node) -> Node:
-    print("new: ", new_node.key)
-    # y = 
+def rb_insert(root: Node, new_node: Node) -> Node:
     x = root
     while x != None:
-        # print("x: ", x.key, root.parent.key)
         y = x
         if new_node.key < x.key:
             x = x.left
@@ -159,29 +137,55 @@ def rb_insert(root: Node, new_node: Node, nil: Node) -> Node:
     return root
 
 
-def rb_tree(data: list):
-    # print(data)
-    nil = Node("nil", -1, None, None, None)
+def test_tree(node: Node, node_list: list, node_len_list: list) -> Node:
+    if node.left is None and node.right is None:
+        if node.color == "gray":
+            node_list.append(node.key)
+        node_len_list.append(len(node_list))
+        return
+    elif node.left is not None and node.right is not None:
+        if node.color == "gray":
+            node_list.append(node.key)
+        test_tree(node.left, copy.copy(node_list), node_len_list)
+        test_tree(node.right, copy.copy(node_list), node_len_list)
+    elif node.left is not None:
+        if node.color == "gray":
+            node_list.append(node.key)
+        test_tree(node.left, copy.copy(node_list), node_len_list)
+    else:
+        if node.color == "gray":
+            node_list.append(node.key)
+        test_tree(node.right, copy.copy(node_list), node_len_list)
+
+
+
+def rb_tree(data: list) -> None:
     root = Node("gray", data[0], None, None, None)
     for i in range(1, len(data)):
         new_node = Node("red", data[i], None, None, None)
-        root = rb_insert(root, new_node, nil)
-    print("root: ", root.key)
-    # print("root.right: ", root.right.key)
-    # print("root.left: ", root.left.key)
+        root = rb_insert(root, new_node)
 
-    # print(root.right.right.right.key)
-    graph = pydot.Dot(graph_type = 'graph', strict=True)
-    x = pydot.Node(root.key, style="filled", fillcolor=root.color)
-    graph.add_node(x)
-    draw_tree(graph, root)
+    # Draw tree graph
+    # graph = pydot.Dot(graph_type = 'graph', strict=True)
+    # x = pydot.Node(root.key, style="filled", fillcolor=root.color)
+    # graph.add_node(x)
+    # draw_tree(graph, root)
+    node_list = []
+    node_len_list = []
+    test_tree(root, node_list, node_len_list)
+    for i in range(1, len(node_len_list)):
+        if node_len_list[i - 1] != node_len_list[i]:
+            print("Error!!!!")
+            break
+    print("TEST OK: ", data)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
 def main():
-    list2 = [7, 2, 14, 5, 1, 8, 11, 4, 15, 17, 18, 19, 20, 21, 23, 24, 25, 30, 35]
-    # list2 = [7, 2, 14, 25, 12, 13, 11, 20, 15, 30]
-    # list2 = [11, 2, 1, 7, 5, 8, 14, 15, 4]
-    rb_tree(list2)
+    for i in range(10000):
+        random_list = random.sample(range(10000), 100)
+        rb_tree(random_list)
+
 
 
 if __name__ == '__main__':
